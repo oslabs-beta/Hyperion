10acbbb0635a1ef5436e2f9b7757e45061d62103
NAusintO <nickaozawa@gmail.com>
2022-02-14 12:01:40 -0800

    intial commit. setting up dev enviornments

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'views', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // <--------- might be syntax that includes both .js and .jsx 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js'],
+  },
+};
