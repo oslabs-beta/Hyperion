fe0e65a2337db4f29d98aaddbdd142dc56b584e9
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-02-21 18:20:09 -0800

    new query box renders

--- client/components/NewQueryWindow.jsx
+++ client/components/NewQueryWindow.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { FormControl, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const NewQueryWindow = () => {
 return (
 <StyledWindow>
       <h4>Enter Query Here</h4> 
+      <form className='new-query-form'>
+      <TextField multiline rows={12} maxRows={12} />
+      <Button type='submit' size='small' variant='contained'>Submit</Button>
+      </form>
 </StyledWindow>
 )
 }
@@ -15,7 +21,7 @@ const StyledWindow = styled.div`
   display: flex;
   flex-direction: column; 
   row-gap: 1.5em;
-  .new-db-form {
+  .new-query-form {
     display: flex; 
     flex-direction: column;
     row-gap: 1em;
