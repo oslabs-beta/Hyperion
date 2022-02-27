26e9c11a73c4f6eed8ffdeb8d1b4f566f26aa1e0
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-02-26 19:37:59 -0800

    completed sign up component

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,39 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FormControl, TextField, Button } from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
 
 // State management 
 
-
+// Login Component
 const Login = () => {
+//Need validation here to see if user has entered valid login credentials 
+// If user enters valid login credentials need to navigate to home page? 
+//function to handle modal open 
+
     return (
+    <LoginArea>
     <LoginBox>
+        <H3> Login </H3>
         <Label>
-        <TextField
-          required id="outlined-required" label="Required"defaultValue="Username "
-        />
+        <TextField required id="outlined-required" label="Username"/>
         </Label>
         <Label>
-         <TextField
-          required id="outlined-required" label="Required"defaultValue="Password "
-        />
+         <TextField id="outlined-password-input" label="Password" type="Password"/>
         </Label>
         <Button  variant='outlined' size='small' color= 'secondary'>LOG IN</Button>
     </LoginBox>
+    <LoginBox>
+    <H3> Don't have an account?  </H3>
+    <Button  variant='outlined' size='small' color= 'secondary' >SIGN UP</Button>
+    </LoginBox>
+    </LoginArea>
     )
+//SignUp Box
+// Should render on-click of signup button 
 }
 
+
+
 //Style Components Here 
+
+const LoginArea = styled.body`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
 
 const LoginBox = styled.form`
   display: flex;
   justify-content: space-around;
   flex-direction: column;
-  margin:100px 100px 0;
+  margin:50px 50px 0;
   width: 25%;
+  border: 1px solid rgba(24, 63, 161, 0.3);
   border-radius: 5px;
   padding: 15px;
-  background-color: rgba(237, 220, 242, 0.8);
 `;
 
 const Label = styled.label`
@@ -41,13 +60,14 @@ const Label = styled.label`
   flex-direction: column;
   padding:10px ;
 `;
-/*
-// Username
-<input type="text" name="username" />
-</Label>
-<Label>
- Password
-<input type="text" name="password" />
-*/ 
+
+const H3 = styled.h3`
+  font-family: 'Notable', sans-serif;
+  font-size: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 
 export default Login
---
+++ client/components/SignUpForm.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TextField, Button } from '@mui/material';
+
+const SignUpForm = () => {
+    return (
+        <SignUpArea> 
+            <SignUpBox>
+            <Label>
+            <TextField label="First Name"/>
+            </Label>
+            <Label>
+            <TextField label="Last Name"/>
+            </Label>
+            <Label>
+            <TextField label="Email"/>
+            </Label>
+            <Label>
+            <TextField label="Username"/>
+            </Label>
+            <Label>
+            <TextField label="Password"/>
+            </Label>
+            <ButtonGroup>
+            <Button  variant='outlined' size='small' color= 'secondary' >CANCEL</Button>
+            <Button  variant='outlined' size='small' color= 'secondary' >SIGN UP</Button>
+            </ButtonGroup>
+            </SignUpBox>
+        </SignUpArea>
+    )
+}
+
+//Style Components Here 
+
+const SignUpArea = styled.body`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
+
+const SignUpBox = styled.form`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  margin:50px 50px 0;
+  width: 25%;
+  border: 1px solid rgba(24, 63, 161, 0.3);
+  border-radius: 5px;
+  padding: 15px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding:10px ;
+`;
+
+const ButtonGroup = styled.label`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding:10px ;
+`;
+
+
+export default SignUpForm
