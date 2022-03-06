e596b50c191c498a76fd0386f09d6c24e777a59d
Chloe Angel <alexis.angel99@gmail.com>
2022-03-05 20:08:43 -0500

    Update SECURITY.md

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 ### This script will NOT work unless you have enabled SSH key authentication for your machine 
 ### To enable SSH key authentication for your machine, you will need to upload your SSH public key to the server
 
-echo "cd /home/hyperion/hyperion/; git pull origin; pm2 restart server;" | ssh hyperion@hyperionapp.com 
+echo "cd ~/hyperion/; git pull origin; pm2 restart server;" | ssh hyperion@hyperionapp.com 
