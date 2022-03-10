d13f0044125bd5c7473cb46a5264a7affb1b03b9
NAusintO <nickaozawa@gmail.com>
2022-03-10 15:58:56 -0500

    changing box plot to get both runtimes

--- client/components/Charts/BoxPlot.tsx
+++ client/components/Charts/BoxPlot.tsx
@@ -8,39 +8,36 @@ import { RunTestResponse } from '../../models/api';
 const BoxPlot = (props: Props) => {
 
   const data = props.data;
-  console.log(data);
 
-  const explainAnalyzeResults = data.testData.filter((response) => { response.method === 'EXPLAIN'});
-  const actualResults = data.testData.filter((response) => { response.method === 'QUERY' });
+  const filter = (array, string) => {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].method === string) {
+        output.push(array[i]);
+      }
+    }
+    return output; 
+  }
+
+
+  // const explainAnalyzeResults = data.testData.filter((response) => { response.method === 'EXPLAIN'});
+  // const actualResults = data.testData.filter((response) => { response.method === 'QUERY' });
+  const explainAnalyzeResults = filter(data.testData, 'EXPLAIN');
+  const actualResults = filter(data.testData, 'QUERY');
+
 
   return (
     <Plot
     data={[
     {
     type: 'box', 
-    name: 'Summary Stats',
-    y: [
-      data.summaryStats.min,
-      data.summaryStats.q1,
-      data.summaryStats.median,
-      data.summaryStats.q3,
-      data.summaryStats.max
-    ],
+    name: 'Explain Analyze',
+    y: explainAnalyzeResults.map((item, i) => { return item.queryTime }),
     marker: {
         color: 'rgb(163, 210, 202)'
       },
       boxmean: 'sd'
     },
-    
-    // {
-    // type: 'box', 
-    // name: 'Explain Analyze',
-    // y: explainAnalyzeResults.map((item) => { return item.queryTime }),
-    // marker: {
-    //     color: 'rgb(240, 89, 69)',
-    //   },
-    //   boxmean: 'sd'
-    // },
     ]}
 
     layout= {{
